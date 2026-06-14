9a93092e00966b4aa9a74a9e11b8a99233f13e12
salimi53as-ctrl <salimi53as@gmail.com>
2026-06-14 04:08:05 +0330

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 BOT_TOKEN = "19108680:VLIdd-6KJY_joTrmPwsTXkIXGVh9pgFs6lM"
 BASE_URL = f"https://tapi.bale.ai/bot{BOT_TOKEN}"
 
-ADMIN_ID = None
+ADMIN_ID = 848341355
 
 # ================= DB =================
 conn = sqlite3.connect("players.db", check_same_thread=False)
